c0ac37fcb18b1841bd0ff61f63bda4e86fe883f9
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2025-08-21 14:25:25 -1000

    Update the CORS rule

--- api/index.js
+++ api/index.js
@@ -1,19 +1,36 @@
 const express = require('express');
+const {Pool}  = require('pg');
 
 const app = express();
-
 const PORT = 3000;
+
+const allowedOrigins = [
+    'http://localhost:8080',
+    'https://www.your-production-domain.com', // Your future website
+    'file:///C:/Users/mkara/Documents/CodingProjects/inspector-tools/ui/index.html'
+]
+
+// Pool manages multiple connections.
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'inspector-tools-db',
+    password: '3531C45499',
+    port: 5432,
+});
 
 app.get('/',  (req, res) => {
     res.send("Hello from the backend!");
 });
 
-app.get('/api/questions', (req, res) => {
-    const sampleQuestions = [
-        { id: 1, text: "This is the first question"},
-        { id: 2, text: "This is the second question"}
-    ];
-    res.json(sampleQuestions);
+app.get('/api/questions', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM questions');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
 
 app.listen(PORT, () => {
